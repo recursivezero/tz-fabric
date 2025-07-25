4c24200091f61d2650d6767590826fa5c1f4d5fe
aadityasarda <adityasarda2004@gmail.com>
2025-07-25 06:09:15 +0530

    deleted frontend-fabric

--- frontend-fabric/src/components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import "../styles/Loader.css";
-
-const Loader = () => {
-  return <div className="loader"></div>;
-};
-
-export default Loader;
--- frontend-fabric/src/utils/imageUtils.js
+++
@@ -1,5 +0,0 @@
-export async function fetchImageAsFile(path, filename) {
-  const res = await fetch(path);
-  const blob = await res.blob();
-  return new File([blob], filename, { type: blob.type });
-}
